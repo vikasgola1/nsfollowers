1dac8f98892547f0acf3b47fd98c7fd1a8c079ee
vikasgola1 <vk0673895@gmail.com>
2024-05-19 10:56:34 +0530

    Update Ns_Followers.py

--- Ns_Followers.py
+++ Ns_Followers.py
@@ -11,9 +11,9 @@ class Ns_Followers:
         self.requests=requests
         self.__AES_NoPadding = AES_CBC_NoPadding()
 
-        self.__userid='...' #Enter UserID Here
-        self.__username='...' #Enter UserName Here
-        self._session='...' #Enter SessionID Here
+        self.__userid='66268528242' #Enter UserID Here
+        self.__username='mahjoub.armin_3089' #Enter UserName Here
+        self._session='66268528242:6ZFlGIMZmzcF7P:27:AYfl0g6-trT-Rv8ctGYdFvlb_fcj9F7NHT5NOlgOQA' #Enter SessionID Here
 
         self.__Pk=self.__userid.encode()+b'*'+str(random.randint(111,999)).encode()
         self.__Ip=os.urandom(8).hex().encode()+b'*'+str(random.randint(111,999)).encode()
